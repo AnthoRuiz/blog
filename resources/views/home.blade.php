cd472b644105464d4d97382ca00ab1d06a5fff08
Anthony Ruiz <yobakaj@gmail.com>
2015-09-15 01:41:10 -0430

    primer commit con elixir

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('content')
+    <h1>Probando</h1>
+    <a class="btn btn-primary" href="#">Crear Post</a>
+    <hr>
+@stop
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Blog Jaka</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+        @yield('content')
+    </div>
+</body>
+</html>
