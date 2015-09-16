b6ec305f1b29df13154c3bf02307c2c1588f3af0
Anthony Ruiz <yobakaj@gmail.com>
2015-09-16 00:48:24 -0430

    un poquito de formato en la vista post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.default')
 
 @section('content')
+
     <h1>{{ $post->title }}</h1>
-    <p>
-        {{ $post->body  }}
-    </p>
+    <br>
+    <p class="lead">{{ $post->body  }} </p>
 @stop
